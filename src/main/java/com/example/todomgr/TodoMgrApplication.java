1b79d0773435f077ca74b807bac6bc4f517b7049
Maksym Govorischev <mgovorischev@gmail.com>
2018-05-23 17:53:07 +0300

    Switch to mongodb and its declarative repositories

--- src/main/java/com/example/todomgr/TodoMgrApplication.java
+++ src/main/java/com/example/todomgr/TodoMgrApplication.java
@@ -2,28 +2,14 @@ package com.example.todomgr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 
 @SpringBootApplication
-@EnableRedisRepositories
+@EnableMongoRepositories
 public class TodoMgrApplication {
     public static void main(String[] args) {
         SpringApplication.run(TodoMgrApplication.class, args);
-    }
-
-    @Bean
-    public LettuceConnectionFactory getConnectionFactory() {
-        return new LettuceConnectionFactory(new RedisStandaloneConfiguration());
-    }
-
-    @Bean
-    public StringRedisTemplate getRedisTemplate(LettuceConnectionFactory connectionFactory) {
-        return new StringRedisTemplate(connectionFactory);
     }
 
 }
--- src/main/java/com/example/todomgr/model/Todo.java
+++ src/main/java/com/example/todomgr/model/Todo.java
@@ -2,18 +2,14 @@ package com.example.todomgr.model;
 
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.redis.core.RedisHash;
-import org.springframework.data.redis.core.index.Indexed;
 
 
-@RedisHash("todos")
 public class Todo {
     @Id
     private String id;
     private String title;
     private String description;
 
-    @Indexed
     private int priority;
 
     public String getId() {
