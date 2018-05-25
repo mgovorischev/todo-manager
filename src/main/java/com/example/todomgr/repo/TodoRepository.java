9a0df1eddb6afc053059fd934392d1cea59bfa32
Maksym Govorischev <mgovorischev@gmail.com>
2018-05-26 00:49:09 +0300

    Introduce Spring Data REST

--- src/main/java/com/example/todomgr/repo/TodoRepository.java
+++ src/main/java/com/example/todomgr/repo/TodoRepository.java
@@ -3,9 +3,11 @@ package com.example.todomgr.repo;
 
 import com.example.todomgr.model.Todo;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource(collectionResourceRel = "todos", path = "todos")
 public interface TodoRepository extends CrudRepository<Todo, String> {
     List<Todo> findByPriority(int priority);
 }
